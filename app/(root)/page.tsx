b35c3b6a2a21df3cc8dc36c38ad282938b35800d
Yanis Ramssamy <ramssamyyanis@gmail.com>
2025-11-10 17:16:43 +0100

    implement header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="flex min-h-screen items-center justify-center text-gray-100 ">
+            Home
+        </div>
+    )
+}
+
+export default Home
---
+++ lib/constants.ts
@@ -0,0 +1,7 @@
+export const NAV_ITEMS : {  href: string; label: string;}[] =[
+    {href : '/', label : "dashboard"},
+    {href : '/search', label : "Search"},
+    {href : '/watchlist', label: "Watchlist"},
+
+
+]
