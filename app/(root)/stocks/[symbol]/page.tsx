633a1b9e9e560f6348e8bc147b41b4897abca085
Yanis Ramssamy <ramssamyyanis@gmail.com>
2026-03-19 16:59:41 +0100

    implementer le détail des stocks

--- app/(root)/stocks/[symbol]/page.tsx
+++ app/(root)/stocks/[symbol]/page.tsx
@@ -8,59 +8,63 @@ import {
     COMPANY_PROFILE_WIDGET_CONFIG,
     COMPANY_FINANCIALS_WIDGET_CONFIG,
 } from "@/lib/constants";
+
 export default async function StockDetails({ params }: StockDetailsPageProps) {
     const { symbol } = await params;
     const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`;
 
     return (
-        <div className="flex min-h-screen home-wrapper">
-            <section className="grid w-full gap-8 home-section">
+        <div className="min-h-screen px-4 py-6 md:px-6 lg:px-8">
+            <section className="w-full max-w-7xl mx-auto">
+                <div className="grid grid-cols-1 md:grid-cols-12 gap-6 md:gap-8">
+                    {/* Left column */}
+                    <div className="col-span-12 md:col-span-8 flex flex-col gap-6">
+                        <TradingViewWidget
+                            scriptUrl={`${scriptUrl}symbol-info.js`}
+                            config={SYMBOL_INFO_WIDGET_CONFIG(symbol)}
+                            height={170}
+                        />
 
-                {/* Left column — col-span-1 comme dans Home */}
-                <div className="flex flex-col gap-6 min-w-0 md:col-span-1 xl:col-span-1">
-                    <TradingViewWidget
-                        scriptUrl={`${scriptUrl}symbol-info.js`}
-                        config={SYMBOL_INFO_WIDGET_CONFIG(symbol)}
-                        height={170}
-                    />
                     <TradingViewWidget
                         scriptUrl={`${scriptUrl}advanced-chart.js`}
                         config={CANDLE_CHART_WIDGET_CONFIG(symbol)}
                         className="custom-chart"
                         height={600}
                     />
+
                     <TradingViewWidget
                         scriptUrl={`${scriptUrl}advanced-chart.js`}
                         config={BASELINE_WIDGET_CONFIG(symbol)}
                         className="custom-chart"
                         height={600}
                     />
-                </div>
+                    </div>
 
-                {/* Right column — col-span-2 comme dans Home */}
-                <div className="flex flex-col gap-6 min-w-0 md:col-span-1 xl:col-span-2">
-                    <WatchlistButton
-                        symbol={symbol.toUpperCase()}
-                        company={symbol.toUpperCase()}
-                        isInWatchlist={false}
-                    />
-                    <TradingViewWidget
+                    {/* Right column */}
+                    <div className="col-span-12 md:col-span-4 flex flex-col gap-6">
+                        <div className="flex items-center justify-between">
+                            <WatchlistButton symbol={symbol.toUpperCase()} company={symbol.toUpperCase()} isInWatchlist={false} />
+                        </div>
+
+                        <TradingViewWidget
                         scriptUrl={`${scriptUrl}technical-analysis.js`}
                         config={TECHNICAL_ANALYSIS_WIDGET_CONFIG(symbol)}
                         height={400}
                     />
+
                     <TradingViewWidget
                         scriptUrl={`${scriptUrl}company-profile.js`}
                         config={COMPANY_PROFILE_WIDGET_CONFIG(symbol)}
                         height={440}
                     />
+
                     <TradingViewWidget
                         scriptUrl={`${scriptUrl}financials.js`}
                         config={COMPANY_FINANCIALS_WIDGET_CONFIG(symbol)}
                         height={464}
                     />
+                    </div>
                 </div>
-
             </section>
         </div>
     );
